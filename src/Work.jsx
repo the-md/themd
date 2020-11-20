d5227389c9875fdc8f6647a340e82347a6e39c1f
Themd <themd@mail.ru>
2020-11-20 08:52:42 +0200

    update 20.11.20

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,72 +1,73 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
-// const workJson = {
-//   "lists": [
-//     {
-//       "id": 1,
-//       "image": "images/cases/sunsago.jpg",
-//       "url": "//sunsago.ru/",
-//       "name": "Sunsago"
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/cyprus.jpg",
-//       "url": "//cyprusforyou.store/",
-//       "name": "Cyprus for you"
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/hartz.jpg",
-//       "url": "//harztgroup.ru/",
-//       "name": "Группа компаний &quot;HARZT&quot;"
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/okna.jpg",
-//       "url": "//kompozitnie-okna.ru/",
-//       "name": "Окна &quot;THERMOBREAK&quot;",
-//       "on": false
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/pskbor.jpg",
-//       "url": "//pskbor.ru/",
-//       "name": "ПСК &quot;Бор&quot;"
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/bigmoscow.jpg",
-//       "url": "//big-moscow.ru/",
-//       "name": "BigMoscow",
-//       "on": false
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/stroitelstvo.jpg",
-//       "url": "",
-//       "name": "Timfort"
-//     }, {
-//       "id": 1,
-//       "image": "images/cases/cpec.jpg",
-//       "url": "//cpec.online/",
-//       "name": "Cpec"
-//     }
-//   ]
-// }
+class Work extends Component {
+  state = {
+    work: [
+      {
+        "image": "images/cases/sunsago.jpg",
+        "url": "//sunsago.ru/",
+        "name": "Sunsago"
+      }, {
+        "image": "images/cases/cyprus.jpg",
+        "url": "//cyprusforyou.store/",
+        "name": "Cyprus for you"
+      }, {
+        "image": "images/cases/hartz.jpg",
+        "url": "//harztgroup.ru/",
+        "name": "Группа компаний \"HARZT\""
+      }, {
+        "image": "images/cases/okna.jpg",
+        "url": "//kompozitnie-okna.ru/",
+        "name": "Окна \"THERMOBREAK\"",
+        "on": false
+      }, {
+        "id": 1,
+        "image": "images/cases/pskbor.jpg",
+        "url": "//pskbor.ru/",
+        "name": "ПСК \"Бор\""
+      }, {
+        "image": "images/cases/bigmoscow.jpg",
+        "url": "//big-moscow.ru/",
+        "name": "BigMoscow",
+        "on": false
+      }, {
+        "image": "images/cases/stroitelstvo.jpg",
+        "url": "",
+        "name": "Timfort",
+        "on": false
+      }, {
+        "image": "images/cases/cpec.jpg",
+        "url": "//cpec.online/",
+        "name": "Cpec",
+        "on": false
+      }
+    ]
+  }
 
-function Work() {
-  return (<div id="main">
+  render() {
 
-    <article id="work">
-      <h2 className="major">Work</h2>
-      <WorkItem image="images/cases/sunsago.jpg" url="//sunsago.ru/" name="Sunsago"/>
-      <WorkItem image="images/cases/cyprus.jpg" url="//cyprusforyou.store/" name="Cyprus for you"/>
-      <WorkItem image="images/cases/hartz.jpg" url="//harztgroup.ru/" name="Группа компаний &quot;HARZT&quot;"/>
-      <WorkItem image="images/cases/pskbor.jpg" url="//pskbor.ru/" name="ПСК &quot;Бор&quot;"/>
+    const work = this.state.work
 
-      <Link to="/" className="close">Close</Link>
-    </article>
-  </div>)
+    return (<div id="main">
+
+      <article id="work">
+        <h2 className="major">Work</h2>
+        <WorkItem image={work[0].image} url={work[0].url} name={work[0].name}/>
+        <WorkItem image={work[1].image} url={work[1].url} name={work[1].name}/>
+        <WorkItem image={work[2].image} url={work[2].url} name={work[2].name}/>
+        <WorkItem image={work[4].image} url={work[4].url} name={work[4].name}/>
+
+        <Link to="/" className="close">Close</Link>
+      </article>
+    </div>)
+  }
 }
 function WorkItem(props) {
-  return (<a href={props.url} className="work__item" target="_blank">
-    <span className="image main"><img src={props.image} alt=""/></span>
+  return (<a href={props.url} className="work__item" target="_blank" rel="noopener noreferrer">
+    <span className="image main">
+      <img src={props.image} alt=""/>
+    </span>
     <h3 className="cases">
       {props.name}
     </h3>
